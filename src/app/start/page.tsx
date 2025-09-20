b0b211107a83d91b4fd04b6e815e2bf301b77d10
honokakimura <2495090t@stu.kobe-u.ac.jp>
2025-09-20 23:17:43 +0900

    fetchProjectのインポートをコメントアウトし、プロジェクト一覧の説明を修正

--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import StartTask from "@/app/start/_components/startTask";
 import Header from "@/app/components/header";
+// import fetchProject from "@/app/lib/fetchProject"
 
 type Project = {
     id: string;
@@ -12,7 +13,8 @@ type Project = {
 export default function Home() {
     const [selectedProjectId, setSelectedProjectId] = useState<`${string}-${string}-${string}-${string}-${string}` | "">("");
 
-    // サンプルのプロジェクト一覧（実際にはサーバーから取得するなど）
+    // サンプルのプロジェクト一覧（実際にはサーバーから取得する）
+    // const projects: Project[] = await fetchProject();
     const projects: Project[] = [
         { id: "", name: "プロジェクトを選択" },
         { id: "11111111-1111-1111-1111-111111111111", name: "プロジェクトA" },
